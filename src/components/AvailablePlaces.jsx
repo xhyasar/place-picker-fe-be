2ac5d21a27f41d50d5c941f81cd305b458f16a7d
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-05-09 01:38:30 +0300

    Add loading state and dynamic places fetching logic
    Introduced loading state and fetching logic in `AvailablePlaces` to retrieve data from the API. Updated `Places` to handle loading and fallback states dynamically, enhancing user experience during data fetching.

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,12 +1,33 @@
+import {useState, useEffect} from 'react';
+
 import Places from './Places.jsx';
 
-export default function AvailablePlaces({ onSelectPlace }) {
-  return (
-    <Places
-      title="Available Places"
-      places={[]}
-      fallbackText="No places available."
-      onSelectPlace={onSelectPlace}
-    />
-  );
+const places = localStorage.getItem('places');
+
+export default function AvailablePlaces({onSelectPlace}) {
+    const [isFetching, setIsFetching] = useState(false);
+    const [availablePlaces, setAvailablePlaces] = useState([]);
+
+    useEffect(() => {
+        async function fetchPlaces() {
+            setIsFetching(true);
+            const response = await fetch('http://localhost:3000/places');
+            const resData = await response.json();
+            setAvailablePlaces(resData.places);
+            setIsFetching(false);
+        }
+        fetchPlaces();
+    }, []);
+
+
+    return (
+        <Places
+            title="Available Places"
+            places={availablePlaces}
+            isLoading={isFetching}
+            loadingText="Loading available places..."
+            fallbackText="No places available."
+            onSelectPlace={onSelectPlace}
+        />
+    );
 }
