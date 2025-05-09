45be3055a11509ff271f00c26730c9f360c59a51
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-05-09 18:07:58 +0300

    Add HTTP utility functions and integrate API calls
    Introduce `fetchAvailablePlaces` and `updateUserPlaces` functions in `http.js` to encapsulate API logic. Refactor components to use these functions, reducing code duplication and improving maintainability. Update `user-places.json` with an initial entry for testing.

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,9 +1,8 @@
 import {useState, useEffect} from 'react';
-
 import Places from './Places.jsx';
 import Error from './Error.jsx';
 import {sortPlacesByDistance} from "../loc.js";
-
+import {fetchAvailablePlaces} from "../http.js";
 
 export default function AvailablePlaces({onSelectPlace}) {
     const [isFetching, setIsFetching] = useState(false);
@@ -16,16 +15,12 @@ export default function AvailablePlaces({onSelectPlace}) {
             setIsFetching(true);
 
             try {
-                const response = await fetch('http://localhost:3000/places');
-                const resData = await response.json();
-
-                if (!response.ok) {
-                    throw new Error('Could not fetch places.');
-                }
+                const places = await fetchAvailablePlaces();
 
                 navigator.geolocation.getCurrentPosition((position) => {
+
                     const sortedPlaces = sortPlacesByDistance(
-                        resData.places,
+                        places,
                         position.coords.latitude,
                         position.coords.longitude);
                     setAvailablePlaces(sortedPlaces);
@@ -38,7 +33,6 @@ export default function AvailablePlaces({onSelectPlace}) {
             }
             setIsFetching(false);
         }
-
         fetchPlaces();
     }, []);
 
