e94a4e7537c4047234a750cfaff4ff847d6ab327
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-05-09 15:12:20 +0300

    Add error handling to AvailablePlaces and create Error component
    Introduced an Error component to display error messages and integrated it into AvailablePlaces for better error handling. The fetch logic now uses try-catch to catch and display errors when fetching places fails. This improves UX by providing clear feedback to users.

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,23 +1,41 @@
 import {useState, useEffect} from 'react';
 
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
-const places = localStorage.getItem('places');
 
 export default function AvailablePlaces({onSelectPlace}) {
     const [isFetching, setIsFetching] = useState(false);
     const [availablePlaces, setAvailablePlaces] = useState([]);
+    const [error, setError] = useState();
+
 
     useEffect(() => {
         async function fetchPlaces() {
             setIsFetching(true);
-            const response = await fetch('http://localhost:3000/places');
-            const resData = await response.json();
-            setAvailablePlaces(resData.places);
+
+            try {
+                const response = await fetch('http://localhost:3000/places');
+                const resData = await response.json();
+
+                if (!response.ok) {
+                    throw new Error('Could not fetch places.');
+                }
+                setAvailablePlaces(resData.places);
+            } catch (error) {
+                setError({
+                    message: error.message || 'Could not fetch places.',
+                });
+            }
             setIsFetching(false);
         }
         fetchPlaces();
     }, []);
+
+
+    if (error) {
+        return < Error title="An error occured!" message={error.message}/>
+    }
 
 
     return (
