dcb32abcc95dcadfd637ed672519e6d393d01318
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-05-09 15:53:32 +0300

    Sort available places by distance using user's location.
    Integrated `sortPlacesByDistance` to prioritize places based on proximity. Fetching logic now utilizes `navigator.geolocation` to obtain the user's coordinates for sorting. Ensured error handling remains intact during location-based updates.

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import {useState, useEffect} from 'react';
 
 import Places from './Places.jsx';
 import Error from './Error.jsx';
+import {sortPlacesByDistance} from "../loc.js";
 
 
 export default function AvailablePlaces({onSelectPlace}) {
@@ -21,7 +22,15 @@ export default function AvailablePlaces({onSelectPlace}) {
                 if (!response.ok) {
                     throw new Error('Could not fetch places.');
                 }
-                setAvailablePlaces(resData.places);
+
+                navigator.geolocation.getCurrentPosition((position) => {
+                    const sortedPlaces = sortPlacesByDistance(
+                        resData.places,
+                        position.coords.latitude,
+                        position.coords.longitude);
+                    setAvailablePlaces(sortedPlaces);
+                    setIsFetching(false);
+                });
             } catch (error) {
                 setError({
                     message: error.message || 'Could not fetch places.',
@@ -29,6 +38,7 @@ export default function AvailablePlaces({onSelectPlace}) {
             }
             setIsFetching(false);
         }
+
         fetchPlaces();
     }, []);
 
