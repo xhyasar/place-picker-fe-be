b638b3b137ceedfc2a41c068c647d47c4a79cd17
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-05-12 21:44:49 +0300

    Refactor fetching logic by introducing useFetch hook
    Replaced repetitive fetching logic in multiple components with a reusable useFetch custom hook. This simplifies the code, improves readability, and reduces duplication. Also updated user-places.json with new locations.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useRef, useState, useCallback, useEffect} from 'react';
+import {useRef, useState, useCallback} from 'react';
 
 import Places from './components/Places.jsx';
 import Modal from './components/Modal.jsx';
@@ -7,31 +7,21 @@ import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
 import {fetchUserPlaces, updateUserPlaces} from "./http.js";
 import Error from './components/Error.jsx';
+import {useFetch} from "./hooks/useFetch.js";
 
 function App() {
     const selectedPlace = useRef();
-
-    const [userPlaces, setUserPlaces] = useState([]);
-    const [isFetching, setIsFetching] = useState(false);
-    const [error, setError] = useState();
 
     const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
 
     const [modalIsOpen, setModalIsOpen] = useState(false);
 
-    useEffect(() => {
-        async function fetchPlaces(){
-            setIsFetching(true);
-            try {
-                const places = await fetchUserPlaces();
-                setUserPlaces(places);
-            } catch (error){
-                setError({message: error.message || 'Fail to fetch user places.'})
-            }
-            setIsFetching(false);
-        }
-        fetchPlaces();
-    }, []);
+    const {
+        isFetching,
+        error,
+        fetchedData: userPlaces,
+        setFetchedData: setUserPlaces
+    } = useFetch(fetchUserPlaces, []);
 
     function handleStartRemovePlace(place) {
         setModalIsOpen(true);
@@ -66,21 +56,22 @@ function App() {
     }
 
     const handleRemovePlace = useCallback(async function handleRemovePlace() {
-        setUserPlaces((prevPickedPlaces) =>
-            prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
-        );
-        try {
-            await updateUserPlaces(
-                userPlaces.filter((place) => place.id !== selectedPlace.current.id)
+            setUserPlaces((prevPickedPlaces) =>
+                prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
             );
-        } catch (error){
-            setUserPlaces(userPlaces);
-            setErrorUpdatingPlaces({
-                message: error.message || 'Failed to delete data.'
-            });
-        }
-        setModalIsOpen(false);
-    }, [userPlaces]);
+            try {
+                await updateUserPlaces(
+                    userPlaces.filter((place) => place.id !== selectedPlace.current.id)
+                );
+            } catch (error) {
+                setUserPlaces(userPlaces);
+                setErrorUpdatingPlaces({
+                    message: error.message || 'Failed to delete data.'
+                });
+            }
+            setModalIsOpen(false);
+        }, [userPlaces, setUserPlaces]
+    );
 
     function handleError() {
         setErrorUpdatingPlaces(null);
@@ -125,7 +116,9 @@ function App() {
                         onSelectPlace={handleStartRemovePlace}
                     />
                 )}
-                <AvailablePlaces onSelectPlace={handleSelectPlace}/>
+                <AvailablePlaces
+                    onSelectPlace={handleSelectPlace}
+                />
             </main>
         </>
     );
